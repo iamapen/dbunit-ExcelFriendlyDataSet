de8ef7a8b02964ea6a4bf94b16ff870404b1e761
kushida <kushida@si-agency.co.jp>
2016-10-21 00:14:44 +0900

    add 1.1.0 deprecated compat dataset

---
+++ src/lib/Iamapen/ExcelFriendlyDataSet/CommentableCsvDataSet.php
@@ -0,0 +1,10 @@
+<?php
+namespace Iamapen\ExcelFriendlyDataSet;
+
+/**
+ * @deprecated 1.1.0
+ * @see \Iamapen\ExcelFriendlyDataSet\Database\DataSet\CommentableCsvDataSet
+ */
+class CommentableCsvDataSet extends Database\DataSet\CommentableCsvDataSet
+{
+}
---
+++ src/lib/Iamapen/ExcelFriendlyDataSet/ExcelCsvDataSet.php
@@ -0,0 +1,10 @@
+<?php
+namespace Iamapen\ExcelFriendlyDataSet;
+
+/**
+ * @deprecated 1.1.0
+ * @see \Iamapen\ExcelFriendlyDataSet\Database\DataSet\ExcelCsvDataSet
+ */
+class ExcelCsvDataSet extends Database\DataSet\ExcelCsvDataSet
+{
+}
